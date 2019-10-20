27d66595abcc53afd288a86c91a50e8adf5a151f
FalsoMoralista <lucianoadfilho@gmail.com>
2019-10-20 20:45:46 -0300

    üp

--- feature_extractor.py
+++ feature_extractor.py
@@ -15,6 +15,7 @@ import os
 from xml.dom import minidom
 import tensorflow as tf
 from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.applications.resnet_v2 import ResNet152V2, preprocess_input
 from keras.preprocessing import image
 from keras.models import Model
 import numpy as np
@@ -36,8 +37,8 @@ if len(argv) < 2:
 else:
     print(tf.__version__) # compatible with tensorflow 1.14
     sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+    base_dir = argv[1]
     if argv[0] == 'resnet50':
-        base_dir = argv[1]
         # Load pre-trained model
         resnet50 = ResNet50(weights='imagenet')
         model = Model(input=resnet50.input, output=resnet50.get_layer('avg_pool').output)
@@ -69,7 +70,37 @@ else:
             line.append(plant_class[0].firstChild.data) # Add its class
             line.append(name) # Add the file name
             csvwriter.writerow(line) # Finally write it down to the csv    
-
+    if argv[0] == 'resnet152v2':
+        resnet152v2 = ResNet152V2(weights='imagenet')
+        model = Model(input=resnet152v2.input, output=resnet152v2.get_layer('avg_pool').output)
+        resnet152v2.trainable = False
+        fields = []
+        for i in range(2048):
+            fields.append('n'+str(i))
+        fields.append('category')
+        fields.append('image name')
+        # Open the csv and append primary fields
+        with open('resnet152v2.csv','w') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            #csvwriter.writerow(fields)
+            for filename in os.listdir(base_dir): # For each image
+                if filename.endswith(".jpg"):
+                    print(filename)
+                    name = filename.replace('.jpg','') # Get its name
+                    doc = minidom.parse(base_dir+'/'+name+'.xml') # Open its xml
+                    plant_class = doc.getElementsByTagName('ClassId') # Get its class
+                    img = image.load_img(base_dir+'/'+filename, target_size=(224,224)) # Load and format to the model expected input size
+                    img_data = image.img_to_array(img) 
+                    img_data = np.expand_dims(img_data, axis=0) # Fit it to a 1D list
+                    img_data = preprocess_input(img_data) # Preprocess accordingly to the model preferences
+                    features = model.predict(img_data) # Get the features
+                    print(features.shape)
+            line = []
+            for x in np.nditer(features): 
+                line.append(str(x)) # Append them to a list format
+            line.append(plant_class[0].firstChild.data) # Add its class
+            line.append(name) # Add the file name
+            csvwriter.writerow(line) # Finally write it down to the csv    
         
 
 
