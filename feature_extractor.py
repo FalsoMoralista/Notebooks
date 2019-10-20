95248059af15fc7f91a740674649cdb88c443452
FalsoMoralista <lucianoadfilho@gmail.com>
2019-10-20 12:20:44 -0300

    done imp

--- feature_extractor.py
+++ feature_extractor.py
@@ -39,8 +39,8 @@ with open('resnet50_features.csv','w') as csvfile:
     csvwriter.writerow(fields)
     for filename in os.listdir('/home/luciano/Desktop/PlantCLEF2013'): # For each image
         if filename.endswith(".jpg"):
+            print(filename)
             name = filename.replace('.jpg','') # Get its name
-            print('File name: %s' %(name))
             doc = minidom.parse('/home/luciano/Desktop/PlantCLEF2013/'+name+'.xml') # Open its xml
             plant_class = doc.getElementsByTagName('ClassId') # Get its class
             img = image.load_img('/home/luciano/Desktop/PlantCLEF2013/'+filename, target_size=(224,224)) # Load and format to the model expected input size
@@ -53,7 +53,6 @@ with open('resnet50_features.csv','w') as csvfile:
                 line.append(str(x)) # Append them to a list format
             line.append(plant_class[0].firstChild.data) # Add its class
             line.append(name) # Add the file name
-            print(line)
             csvwriter.writerow(line) # Finally write it down to the csv    
 
 
